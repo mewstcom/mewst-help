1fc8ca56e2aeaca690302421642be1de09c06e24
Koji Shimba <me@shimba.co>
2024-05-01 07:49:45 +0900

    表示内容を整理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import LinkCard from "@site/src/components/LinkCard";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,7 +15,17 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
+
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs"
+          >
+            ドキュメントを見る
+          </Link>
+        </div>
       </div>
     </header>
   );
@@ -25,11 +35,11 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title=''
+      description={siteConfig.tagline}
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
